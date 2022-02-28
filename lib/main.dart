f22565340ba4c3003be6250ae5f553d86d874e8d
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-02-28 12:49:38 +0700

    change data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'learning/1.scaffold.dart';
+// import 'learning/1.scaffold.dart';
+// import 'learning/2.row-and-column.dart';
+// import 'learning/3.container.dart';
+// import 'learning/4.stack-and-position.dart';
+// import 'learning/5.example-stack-and-position.dart';
+// import 'learning/6.list-view.dart';
+// import 'learning/7.flexbox.dart';
+// import 'learning/8.grid.dart';
 
 void main() {
   runApp(MyApp());
